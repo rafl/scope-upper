c8549c2d49ee305498c19318c51dc6540dba8b3f
Vincent Pit <vince@profvince.com>
2009-01-04 16:53:28 +0100

    This is 0.03

--- lib/Scope/Upper.pm
+++ lib/Scope/Upper.pm
@@ -9,13 +9,13 @@ Scope::Upper - Act on upper scopes.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
 our $VERSION;
 BEGIN {
- $VERSION = '0.02';
+ $VERSION = '0.03';
 }
 
 =head1 SYNOPSIS
