e5ece1843f061be7de19ba4bacdd247e0d05c2b9
Vincent Pit <vince@profvince.com>
2009-01-04 16:39:27 +0100

    Bump copyright year

--- lib/Scope/Upper.pm
+++ lib/Scope/Upper.pm
@@ -212,7 +212,7 @@ Inspired by Ricardo Signes.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Vincent Pit, all rights reserved.
+Copyright 2008-2009 Vincent Pit, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
